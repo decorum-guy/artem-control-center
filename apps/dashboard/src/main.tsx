5887a2ac1e04f13b2ec88f2e318132bf3d547aa4
Easy_Win <65539106+decorum-guy@users.noreply.github.com>
2026-07-31 03:18:56 +0300

    feat: install AVALAR actions provider

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { AccessProvider } from "./AccessControls";
+import { AvalarActionsProvider } from "./AvalarActions";
 import { App } from "./App";
 import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AccessProvider>
-      <App />
+      <AvalarActionsProvider>
+        <App />
+      </AvalarActionsProvider>
     </AccessProvider>
   </StrictMode>
 );
