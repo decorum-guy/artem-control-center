1e57143a0e5b21e386242e962691b6aff4f5449f
Easy_Win <65539106+decorum-guy@users.noreply.github.com>
2026-07-31 03:16:52 +0300

    feat: install access provider in dashboard

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { AccessProvider } from "./AccessControls";
 import { App } from "./App";
 import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <AccessProvider>
+      <App />
+    </AccessProvider>
   </StrictMode>
 );
-
