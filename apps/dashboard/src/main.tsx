5377793871efe90bf4c6970ea674249a415b66d2
Easy_Win <65539106+decorum-guy@users.noreply.github.com>
2026-07-31 03:20:58 +0300

    style: load AVALAR action styles

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { AccessProvider } from "./AccessControls";
 import { AvalarActionsProvider } from "./AvalarActions";
 import { App } from "./App";
+import "./AvalarActions.css";
 import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
