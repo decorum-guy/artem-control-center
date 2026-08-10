f12026f2ee1d92b4a791103e548b2c35d670d770
Easy_Win <65539106+decorum-guy@users.noreply.github.com>
2026-08-11 02:26:46 +0300

    feat: mount connectivity recovery provider

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { AccessProvider } from "./AccessControls";
 import { ActionConfirmationProvider } from "./ActionConfirmations";
 import { AvalarActionsProvider } from "./AvalarActions";
+import { ConnectivityActionsProvider } from "./ConnectivityActions";
 import { App } from "./App";
 import "./ActionConfirmations.css";
 import "./AvalarActions.css";
@@ -12,9 +13,11 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ActionConfirmationProvider>
       <AccessProvider>
-        <AvalarActionsProvider>
-          <App />
-        </AvalarActionsProvider>
+        <ConnectivityActionsProvider>
+          <AvalarActionsProvider>
+            <App />
+          </AvalarActionsProvider>
+        </ConnectivityActionsProvider>
       </AccessProvider>
     </ActionConfirmationProvider>
   </StrictMode>
